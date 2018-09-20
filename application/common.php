eb9dc2f7e60d78c36b8125852e8a146713013364
36D-CUP <35958678+36D-CUP@users.noreply.github.com>
2018-09-20 14:12:50 +0800

    列表已完成
    列表已完成

--- application/common.php
+++ application/common.php
@@ -10,3 +10,47 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+//首页数据      -----------------------------------------------
+    //主数据
+    function listindex($data)
+    {
+        $arr['empty']      = "<tr><td colspan='100' style='text-align:center'>没有任何数据</td></tr>";        //显示空
+        $arr['table']      = $data['table'];            //模板上使用,数据表
+        $arr['con']        = $data['con'];              //模板上使用,控制器
+        $arr['fun']        = $data['fun'];              //模板上使用,方法
+        $arr['data_num']   = $data['showNum'];          //搜索保留值    
+        $arr['pro']        = $data['pro'];              //当前页
+        $arr['row']        = $data['row'];              //遍历数据
+        $arr['title']      = $data['title_txt'];        //子目录
+        $arr['title_txt']  = $data['title'];            //主目录
+        $arr['page']       = $data['page'];             //显示页数
+        $arr['nums']       = $data['nums'];             //共多少条数据
+        $arr['pros']       = $data['pros'];             //分页开始
+        $arr['proe']       = $data['proe'];             //分页结束
+        $arr['s_ty']       = $data['s_ty'];         	//搜索键
+        $arr['s_val']      = $data['s_val'];        	//搜索值
+
+        return $arr;
+    }
+    //分页数据,1.显示页数,2.数据数量,3当前页
+    function pageindex($showNum , $num ,$pro){
+        $arr_page = $showNum == 'x'?1:ceil($num/$showNum);           //获取条数
+        $page = $arr_page;
+        $arr_nums = $num;
+        if($pro-2<=0){
+            $arr_pros = 1;
+            $arr_proe = $page<8?$page+1:8;
+        }else if($pro+1>$page){
+            $arr_pros = $page-4<=0?1:$pro-4;
+            $arr_proe = $page+1;
+        }else{
+            $arr_pros = $pro-2<=0?1:$pro-2;
+            $arr_proe = $page+1;
+        }
+        $arr['arr_page'] = $arr_page;
+        $arr['arr_nums'] = $arr_nums;
+        $arr['arr_pros'] = $arr_pros;
+        $arr['arr_proe'] = $arr_proe;
+        return $arr;
+    }
+//首页数据  end-------------------------------------------------
--- application/myshop_admin/controller/Index.php
+++ application/myshop_admin/controller/Index.php
@@ -7,43 +7,79 @@ use think\Session;
 
 class Index extends Allow
 {
+    //首页
     public function getIndex()
     {
-    	return $this->fetch('Indexpublic/index');
+        return $this->fetch('Indexpublic/index');
     }
 
+    //管理员列表
     public function getAdmin()
     {
-        //分页
-        $pro = input('pro')==""?1:input('pro');                         //获取分页传过来的当前页
-        $showNum = input('data_num')?input('data_num'):'10';             //设定每一个显示的条数
-        $str = (($pro-1)*$showNum).",".$showNum;                        //分页
+        //勿动
+            //分页
+            $pro = input('pro')==""?1:input('pro');                         //获取分页传过来的当前页
+            $showNum = input('data_num')?input('data_num'):'10';            //设定每一个显示的条数
+            $str = $showNum == 'x'?'0,':(($pro-1)*$showNum).",".$showNum;   //分页,x为全部
+            //搜索
+            $s_val  = input('search_value');                                //获取搜索传过来的值
+            $s_ty   = input('search_type');                                 //获取搜索传过来的字段名
+            $s_type = $s_val?input('type'):'其他';                          //获取搜索传过来的搜索类型
+        //勿动
 
-    	$data = Db::table($this->dbt.'admin')->select();
 
-        //分页
-        $num = count($data);
-        $arr['page'] = ceil($num/$showNum);//获取条数
-        $page = $arr['page'];
-        $arr['nums'] = $num;
-        if($pro-2<=0){
-            echo 1;
-            $arr['pros'] = 1;
-            $arr['proe'] = $page<8?$page+1:8;
-        }else if($pro+1>$page){
-            echo 2;
-            $arr['pros'] = $page-4<=0?1:$pro-4;
-            $arr['proe'] = $page+1;
-        }else{
-            echo 3;
-            $arr['pros'] = $pro-2<=0?1:$pro-2;
-            $arr['proe'] = $page+1;
+        $table = $this->dbt.'admin';        //数据表
+
+        switch($s_type){
+            case 1:
+                if($s_ty == 'id'){
+                    $where['id'] = $s_val;
+                }else{
+                    $where[] = [$s_ty,'like', '%' . $s_val . '%'];
+                }
+                
+                $num = Db::table($table)->where($where)->count();                   //数据总数量
+                $data = Db::table($table)->where($where)->limit($str)->select();    //数据
+            break;
+            default:
+                $num = Db::table($table)->count();                                  //数据总数量
+                $data = Db::table($table)->limit($str)->select();                   //数据
+            break;
         }
-        $arr['pro'] = $pro;                                                                                 //当前页
+
+        //分页1.显示页数,2.数据数量,3当前页          封装在common
+        $pageindex = pageindex($showNum , $num ,$pro);
 
 
-    	$arr['row']        = $data;																			//列表数据
-    	return $this->fetch('Admin/admin',$arr);
+        $arr_data = [
+            'table'     => $table,                      //数据表名          
+            'con'       => 'myshopadmin',               //控制器名
+            'fun'       => 'admin',                     //admin
+            'showNum'   => $showNum,                    //显示页数
+            'pro'       => $pro,                        //当前页
+            'row'       => $data,                       //遍历数据
+            'title'     => '管理员管理',                 //主目录名称
+            'title_txt' => '管理员列表',                 //子目录名称
+            'page'      => $pageindex['arr_page'],      //页数
+            'nums'      => $pageindex['arr_nums'],      //共多少条数据
+            'pros'      => $pageindex['arr_pros'],      //分页开始
+            'proe'      => $pageindex['arr_proe'],      //分页结束
+            's_ty'      => $s_ty,                       //搜索键
+            's_val'     => $s_val,                      //搜索值
+        ];
+
+        //组装数据      封装在common
+        $arr = listindex($arr_data);
+
+        return $this->fetch('Admin/admin',$arr);
     }
+
+    //管理员添加
+    public function getAdminadd()
+    {
+        return $this->fetch('Admin/adminadd');
+    }
+
+
 
 }
